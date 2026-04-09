8cd9b14afbdf58f9ca6f1ecf5bcdae6c19c24e89
albashzia <ahmedalbashzia@gmail.com>
2026-04-09 22:11:34 +0500

    Defining the basic methods for the Password class with added necessary headers

--- src/Code/Password.java
+++ src/Code/Password.java
@@ -2,4 +2,37 @@ package Code;
 
 public class Password {
     private String value;
+
+    void setValue(String value){
+        this.value = value;
+    }
+
+    String getValue(){
+        return value;
+    }
+
+    int getLength(){
+        return 0;
+    }
+
+    boolean containsUppercase(){
+        return false;
+    }
+
+    boolean containsLowercase(){
+        return false;
+    }
+
+    boolean containsDigit(){
+        return false;
+    }
+
+    boolean containsSpecialChar(){
+        return false;
+    }
+
+    boolean containsPattern(){
+        return false;
+    }
+
 }
