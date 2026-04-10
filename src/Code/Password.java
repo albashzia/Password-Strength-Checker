8177fb9b45684d5c10e7b74949bf31cce44eb411
albashzia <ahmedalbashzia@gmail.com>
2026-04-10 15:58:15 +0500

    Defining the default and parameterized constructors for Password class

--- src/Code/Password.java
+++ src/Code/Password.java
@@ -3,6 +3,13 @@ package Code;
 public class Password {
     private String value;
 
+    Password(){
+        value = "!QWERTYasdfg1234";
+    }
+
+    Password(String value){
+        this.value = value;
+    }
     void setValue(String value){
         this.value = value;
     }
