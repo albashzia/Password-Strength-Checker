283cca428dc55d0c1ef06b515ce470b118b81bed
albashzia <ahmedalbashzia@gmail.com>
2026-04-17 16:08:22 +0500

    Modifying the loop to correct the IndexOutOfBoundsException and modifying the range to check the special character

--- src/Code/Password.java
+++ src/Code/Password.java
@@ -50,10 +50,10 @@ public class Password {
     }
 
     boolean containsSpecialChar(){
-        for (int index = 0; index <= value.length(); index++){
+        for (int index = 0; index < value.length(); index++){
             if (
                     ((int)value.charAt(index)>=33 &&
-                     (int)value.charAt(index)<=48) ||
+                     (int)value.charAt(index)<=47) ||
                     ((int)value.charAt(index)>=91 &&
                      (int)value.charAt(index)<=96) ||
                     ((int)value.charAt(index)>=123 &&
