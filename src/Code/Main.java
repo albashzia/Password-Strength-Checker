a3f8fade24db37f9566fc1207f5eef75ab397a2b
albashzia <ahmedalbashzia@gmail.com>
2026-04-17 15:43:17 +0500

    Adding body to main method inside Main class to test the implemented methods of Password class

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -2,6 +2,15 @@ package Code;
 
 class Main{
     public static void main() {
+        Password pwd = new Password("QWERTYuiop1234");
 
+        boolean upper = pwd.containsUppercase();
+        System.out.println("Contains Upper case letter: "+upper);
+
+        boolean lower = pwd.containsLowercase();
+        System.out.println("Contains Lower case letter: "+lower);
+
+        boolean digit = pwd.containsDigit();
+        System.out.println("Contains a digit: "+digit);
     }
 }
