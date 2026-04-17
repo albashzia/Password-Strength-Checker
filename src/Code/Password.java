4e5144a10898ad921789c97bac664470c8939381
albashzia <ahmedalbashzia@gmail.com>
2026-04-17 15:59:23 +0500

    Adding body to containsSpecialChar method to loop through the given value and return true if a special character is present in the given String

--- src/Code/Password.java
+++ src/Code/Password.java
@@ -50,6 +50,19 @@ public class Password {
     }
 
     boolean containsSpecialChar(){
+        for (int index = 0; index <= value.length(); index++){
+            if (
+                    ((int)value.charAt(index)>=33 &&
+                     (int)value.charAt(index)<=48) ||
+                    ((int)value.charAt(index)>=91 &&
+                     (int)value.charAt(index)<=96) ||
+                    ((int)value.charAt(index)>=123 &&
+                     (int)value.charAt(index)<=126)
+            )
+            {
+                return true;
+            }
+        }
         return false;
     }
 
