9e1416082afae21b30d12ceac560dbf01c77b348
albashzia <ahmedalbashzia@gmail.com>
2026-04-18 09:02:15 +0500

    Importing Scanner class and creating a Scanner object to take the value from the user

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -1,6 +1,9 @@
 package Code;
+import java.util.Scanner;
 
 class Main{
+    Scanner input = new Scanner(System.in);
+
     public static void main(String args[]) {
         Password pwd = new Password("@QWERTYuiopabcd");
 
