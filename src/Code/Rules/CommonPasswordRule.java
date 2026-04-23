00e5147907dcc64ad6c0924f926a6c20524f458d
albashzia <ahmedalbashzia@gmail.com>
2026-04-23 19:40:34 +0500

    Adding an ArrayList containing common passwords to validate against the given password

--- src/Code/Rules/CommonPasswordRule.java
+++ src/Code/Rules/CommonPasswordRule.java
@@ -1,4 +1,62 @@
 package Code.Rules;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommonPasswordRule extends PasswordRule{
+
+    ArrayList<String> commonPasswords = new ArrayList<>(Arrays.asList(
+            "123456",
+            "123456789",
+            "12345678",
+            "12345",
+            "1234567",
+            "password",
+            "password123",
+            "admin",
+            "admin123",
+            "qwerty",
+            "qwerty123",
+            "abc123",
+            "111111",
+            "123123",
+            "000000",
+            "iloveyou",
+            "welcome",
+            "monkey",
+            "login",
+            "letmein",
+            "football",
+            "dragon",
+            "master",
+            "sunshine",
+            "ashley",
+            "bailey",
+            "shadow",
+            "superman",
+            "harley",
+            "password1",
+            "trustno1",
+            "hello",
+            "freedom",
+            "whatever",
+            "qazwsx",
+            "starwars",
+            "passw0rd",
+            "654321",
+            "7777777",
+            "1q2w3e4r",
+            "zaq12wsx",
+            "google",
+            "facebook",
+            "youtube",
+            "linkedin",
+            "admin@123",
+            "root",
+            "toor",
+            "guest",
+            "test",
+            "user",
+            "default"
+    ));
 }
