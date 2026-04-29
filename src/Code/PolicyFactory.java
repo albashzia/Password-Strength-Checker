07dabe294e14ed111c637a3af53d2386b6df8e90
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 12:45:55 +0500

    Adding the method headers in PolicyFactory class

--- src/Code/PolicyFactory.java
+++ src/Code/PolicyFactory.java
@@ -1,4 +1,11 @@
 package Code;
 
+//Creates policies based on user choice
+
 public class PolicyFactory {
+
+    void createBasicPolicy(){};
+    void createIntermediatePolicy(){};
+    void createStrongPolicy(){};
+    void createAdvancedPolicy(){};
 }
