67684daf9e2f9c0dec8d4501061ad741c1543972
albashzia <ahmedalbashzia@gmail.com>
2026-06-06 16:51:17 +0500

    Overriding the validate method of abstract class PasswordRule in LengthRule class

--- src/Code/Rules/LengthRule.java
+++ src/Code/Rules/LengthRule.java
@@ -1,4 +1,11 @@
 package Code.Rules;
 
+import Code.Password;
+
 public class LengthRule extends PasswordRule{
+
+    @Override
+    boolean validate(Password password) {
+        return false;
+    }
 }
