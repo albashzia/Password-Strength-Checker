f0a5381f930df252ca3776952aa58a399d300982
albashzia <ahmedalbashzia@gmail.com>
2026-07-15 23:35:51 +0500

    Define the layout of HomeFrame

--- src/GUI/HomeFrame.java
+++ src/GUI/HomeFrame.java
@@ -1,11 +1,13 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HomeFrame {
 
     JFrame homeFrame;
     HomeFrame(){
         homeFrame = new JFrame("Password Strength Checker");
+        homeFrame.setLayout(new BorderLayout());
     }
 }
