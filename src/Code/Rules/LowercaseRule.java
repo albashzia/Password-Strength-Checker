df489bc250be37a9222d228136f6bcc58cf8db8f
albashzia <ahmedalbashzia@gmail.com>
2026-07-15 23:43:03 +0500

    Implement abstract class method in LowercaseRule class

--- src/Code/Rules/LowercaseRule.java
+++ src/Code/Rules/LowercaseRule.java
@@ -1,4 +1,13 @@
 package Code.Rules;
 
+import Code.Password;
+
 public class LowercaseRule extends PasswordRule{
+
+
+
+    @Override
+    boolean validate(Password password) {
+        return false;
+    }
 }
