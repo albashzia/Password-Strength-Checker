733abd423a950cefcf54c1f7df7c217b23b03004
albashzia <ahmedalbashzia@gmail.com>
2026-07-15 19:03:28 +0500

    Create constructor of DigitRule class

--- src/Code/Rules/DigitRule.java
+++ src/Code/Rules/DigitRule.java
@@ -1,4 +1,10 @@
 package Code.Rules;
 
+import Code.Password;
+
 public class DigitRule extends PasswordRule{
+
+    public DigitRule(String ruleName, String errorMessage){
+        super(ruleName,errorMessage);
+    }
 }
