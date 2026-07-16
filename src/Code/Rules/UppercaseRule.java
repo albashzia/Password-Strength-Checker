75a100104581f8219df6a416ae6e6caac9e00d30
albashzia <ahmedalbashzia@gmail.com>
2026-07-16 23:22:37 +0500

    Create method in UppercaseRule class to check if the given string contains an upper case letter or not

--- src/Code/Rules/UppercaseRule.java
+++ src/Code/Rules/UppercaseRule.java
@@ -10,6 +10,20 @@ public class UppercaseRule extends PasswordRule{
         this.password = password;
     }
 
+    public boolean hasUpperCase(String password) {
+        boolean upperCase = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            for (char c = 'A'; c <= 'Z'; c++) {
+                if (password.charAt(i) == c) {
+                    upperCase = true;
+                    return upperCase;
+                }
+            }
+        }
+        return upperCase;
+    }
+
     @Override
     boolean validate(Password password) {
         return false;
