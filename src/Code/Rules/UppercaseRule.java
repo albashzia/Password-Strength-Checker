0d3d5fa2204d0f6bd85ebf721b6c007c2b6572cf
albashzia <ahmedalbashzia@gmail.com>
2026-07-16 23:18:40 +0500

    Implement the abstract class methods in UppercaseRule class

--- src/Code/Rules/UppercaseRule.java
+++ src/Code/Rules/UppercaseRule.java
@@ -1,4 +1,12 @@
 package Code.Rules;
 
+import Code.Password;
+
 public class UppercaseRule extends PasswordRule{
+
+
+    @Override
+    boolean validate(Password password) {
+        return false;
+    }
 }
