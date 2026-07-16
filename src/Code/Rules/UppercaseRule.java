a945a3c1fcaf8fe42ecbecc81ccdf31a077ae72d
albashzia <ahmedalbashzia@gmail.com>
2026-07-16 23:20:26 +0500

    Create parameterized constructor of UppercaseRule class

--- src/Code/Rules/UppercaseRule.java
+++ src/Code/Rules/UppercaseRule.java
@@ -4,6 +4,11 @@ import Code.Password;
 
 public class UppercaseRule extends PasswordRule{
 
+    String password;
+    UppercaseRule(String ruleName, String errorMessage, String password){
+        super(ruleName,errorMessage);
+        this.password = password;
+    }
 
     @Override
     boolean validate(Password password) {
