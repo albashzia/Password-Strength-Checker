c9af768eb453990f8e863525182e18655f0f078c
albashzia <ahmedalbashzia@gmail.com>
2026-07-18 23:54:25 +0500

    Define the constructor of SpecialCharacterRule class

--- src/Code/Rules/SpecialCharacterRule.java
+++ src/Code/Rules/SpecialCharacterRule.java
@@ -4,6 +4,10 @@ import Code.Password;
 
 public class SpecialCharacterRule extends PasswordRule{
 
+    public SpecialCharacterRule(String ruleName, String errorMessage){
+        super(ruleName,errorMessage);
+    }
+
     @Override
     public boolean validate(Password password) {
         return false;
