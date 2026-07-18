ef5cdd6e89523aaebc527dd4edb25b929f81c8e7
albashzia <ahmedalbashzia@gmail.com>
2026-07-18 23:52:09 +0500

    Implement the method of abstract class in SequentialPatternRule class

--- src/Code/Rules/SequentialPatternRule.java
+++ src/Code/Rules/SequentialPatternRule.java
@@ -1,4 +1,12 @@
 package Code.Rules;
 
+import Code.Password;
+
 public class SequentialPatternRule extends PasswordRule{
+
+
+    @Override
+    public boolean validate(Password password) {
+        return false;
+    }
 }
