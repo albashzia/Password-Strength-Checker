ed988b770b795bd8a66df27427c6ea589adc3509
albashzia <ahmedalbashzia@gmail.com>
2026-07-18 23:48:23 +0500

    Move the logic to check the presence of Upper Case in Overridden validate method of UppercaseRule class instead of separate method

--- src/Code/Rules/UppercaseRule.java
+++ src/Code/Rules/UppercaseRule.java
@@ -10,22 +10,16 @@ public class UppercaseRule extends PasswordRule{
         this.password = password;
     }
 
-    public boolean hasUpperCase(String password) {
-        boolean upperCase = false;
-
-        for (int i = 0; i < password.length(); i++) {
-            for (char c = 'A'; c <= 'Z'; c++) {
-                if (password.charAt(i) == c) {
-                    upperCase = true;
-                    return upperCase;
-                }
-            }
-        }
-        return upperCase;
-    }
 
     @Override
     boolean validate(Password password) {
+        for (int i = 0; i < password.getValue().length(); i++) {
+            for (char c = 'A'; c <= 'Z'; c++) {
+                if (password.getValue().charAt(i) == c) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
