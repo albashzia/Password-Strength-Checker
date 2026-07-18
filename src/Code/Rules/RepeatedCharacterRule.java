946ed776761462d6edb6767e012f5462654fb833
albashzia <ahmedalbashzia@gmail.com>
2026-07-18 23:45:18 +0500

    Add the constructor of RepeatedCharacterRule class

--- src/Code/Rules/RepeatedCharacterRule.java
+++ src/Code/Rules/RepeatedCharacterRule.java
@@ -4,6 +4,9 @@ import Code.Password;
 
 public class RepeatedCharacterRule extends PasswordRule{
 
+    public RepeatedCharacterRule(String ruleName, String errorMessage){
+        super(ruleName,errorMessage);
+    }
 
     @Override
     public boolean validate(Password password) {
