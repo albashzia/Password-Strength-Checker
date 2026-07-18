4b0119c09161b0559dad3f07a1dae1ef5439a825
albashzia <ahmedalbashzia@gmail.com>
2026-07-18 22:35:50 +0500

    Change the access specifier of constructor and validate method in LowercaseRule class to public

--- src/Code/Rules/LowercaseRule.java
+++ src/Code/Rules/LowercaseRule.java
@@ -4,13 +4,12 @@ import Code.Password;
 
 public class LowercaseRule extends PasswordRule{
 
-    String password;
-    LowercaseRule(String ruleName, String errorMessage){
+    public LowercaseRule(String ruleName, String errorMessage){
         super(ruleName,errorMessage);
     }
 
     @Override
-    boolean validate(Password password) {
+    public boolean validate(Password password) {
         for (int i = 0; i < password.getValue().length(); i++){
             for(char c = 'a'; c <= 'z'; c++){
                 if(password.getValue().charAt(i) == c ){
