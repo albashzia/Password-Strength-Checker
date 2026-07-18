e0cc8ea0f2ed37c1151914bde25d69a95a26006b
albashzia <ahmedalbashzia@gmail.com>
2026-07-18 23:53:45 +0500

    Implement validate method in SpecialCharacterRule class

--- src/Code/Rules/SpecialCharacterRule.java
+++ src/Code/Rules/SpecialCharacterRule.java
@@ -1,4 +1,11 @@
 package Code.Rules;
 
+import Code.Password;
+
 public class SpecialCharacterRule extends PasswordRule{
+
+    @Override
+    public boolean validate(Password password) {
+        return false;
+    }
 }
