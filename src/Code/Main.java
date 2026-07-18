966ef3330c22cd51024565a635eb414e7c4900d8
albashzia <ahmedalbashzia@gmail.com>
2026-07-18 17:02:49 +0500

    Add the statements in main to test the validate method of LengthRule class

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -1,6 +1,7 @@
 package Code;
 import Code.Rules.CommonPasswordRule;
 import Code.Rules.DigitRule;
+import Code.Rules.LengthRule;
 
 import java.io.DataInput;
 import java.util.Scanner;
@@ -9,6 +10,7 @@ class Main{
     static Scanner input = new Scanner(System.in);
     static CommonPasswordRule commonPasswordRule = new CommonPasswordRule("Common Password Rule","Error in CommonPasswordRule class");
     static DigitRule digitRule = new DigitRule("Digit Rule","Error in DigitRule class");
+    static LengthRule lengthRule = new LengthRule("Length Rule", "Error in LengthRule class");
 
     public static void main(String args[]) {
 
@@ -23,14 +25,18 @@ class Main{
         boolean lower = pwd.containsLowercase();
         System.out.println("Contains Lower case letter: "+lower);
 
-        boolean digit = digitRule.validate(pwd);
-        System.out.println("Contains a digit: "+digit);
 
         boolean specialChar = pwd.containsSpecialChar();
         System.out.println("Contains Special Character: "+specialChar);
 
         boolean pattern = pwd.containsPattern();
         System.out.println("Contains Pattern: "+pattern);
+
+        boolean length = lengthRule.validate(pwd);
+        System.out.println("Satisfies Length Requirement: "+length);
+
+        boolean digit = digitRule.validate(pwd);
+        System.out.println("Contains a digit: "+digit);
 
         boolean commonPassword = commonPasswordRule.validate(pwd);
         System.out.println("Contains Common Password: "+commonPassword);
