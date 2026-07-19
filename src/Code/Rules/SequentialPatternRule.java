7768bb964eefeaa2cbb211530a4e77133db4170f
albashzia <ahmedalbashzia@gmail.com>
2026-07-19 15:54:28 +0500

    Move the method to check the presence of sequential pattern for a minimum length of 3 in validate method of SequentialPatternRule class

--- src/Code/Rules/SequentialPatternRule.java
+++ src/Code/Rules/SequentialPatternRule.java
@@ -10,6 +10,37 @@ public class SequentialPatternRule extends PasswordRule{
 
     @Override
     public boolean validate(Password password) {
+        String value = password.getValue();
+
+        String pattern1 = "0123456789";
+        for (int i = 0; i < pattern1.length(); i++){
+            for (int j = i+3; j <= pattern1.length(); j++){
+                String sub = pattern1.substring(i,j);
+                if (value.contains(sub)){
+                    return true;
+                }
+            }
+        }
+
+        String pattern2 = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < pattern2.length(); i++){
+            for (int j = i+3; j <= pattern2.length(); j++){
+                String sub = pattern2.substring(i,j);
+                if (value.contains(sub)){
+                    return true;
+                }
+            }
+        }
+
+        String pattern3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < pattern3.length(); i++){
+            for (int j = i+3; j <= pattern3.length(); j++){
+                String sub = pattern3.substring(i,j);
+                if (value.contains(sub)){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
