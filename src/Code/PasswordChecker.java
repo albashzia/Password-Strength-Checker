91ce5430bddbe6d938a0fadf33d1e18678d74c33
albashzia <ahmedalbashzia@gmail.com>
2026-07-20 23:45:56 +0500

    Create an instance of CommonPasswordRule class in constructor of PasswordChecker class

--- src/Code/PasswordChecker.java
+++ src/Code/PasswordChecker.java
@@ -1,4 +1,10 @@
 package Code;
 
+import Code.Rules.CommonPasswordRule;
+
 public class PasswordChecker {
+
+    PasswordChecker(){
+        CommonPasswordRule commonPasswordRule = new CommonPasswordRule();
+    }
 }
