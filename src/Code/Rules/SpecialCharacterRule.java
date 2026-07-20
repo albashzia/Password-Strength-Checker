e7bdc45069e3e1d2774a19c8de7c7abfcfe78ec3
albashzia <ahmedalbashzia@gmail.com>
2026-07-20 22:47:42 +0500

    Add special character validation logic to validate method of SpecialCharacterRule class

--- src/Code/Rules/SpecialCharacterRule.java
+++ src/Code/Rules/SpecialCharacterRule.java
@@ -10,6 +10,22 @@ public class SpecialCharacterRule extends PasswordRule{
 
     @Override
     public boolean validate(Password password) {
+        String value = password.getValue();
+        for (int index = 0; index < value.length(); index++){
+            if (
+                    (((int) value.charAt(index) >= 33) &&
+                            ((int) value.charAt(index) <= 47)) ||
+                            (((int) value.charAt(index) >= 58) &&
+                                    ((int) value.charAt(index) <= 64)) ||
+                            (((int) value.charAt(index) >= 91) &&
+                                    ((int) value.charAt(index) <= 96)) ||
+                            (((int) value.charAt(index) >= 123) &&
+                                    ((int) value.charAt(index) <= 126))
+            )
+            {
+                return true;
+            }
+        }
         return false;
     }
 }
