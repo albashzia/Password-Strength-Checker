4b45cbca82816a247683939bad220777ef48eb3c
albashzia <ahmedalbashzia@gmail.com>
2026-07-20 22:48:48 +0500

    Remove the containsSpecialCharacter() method from Password class

--- src/Code/Password.java
+++ src/Code/Password.java
@@ -18,24 +18,4 @@ public class Password {
         return value;
     }
 
-
-
-    boolean containsSpecialChar(){
-        for (int index = 0; index < value.length(); index++){
-            if (
-                    ((int)value.charAt(index)>=33 &&
-                     (int)value.charAt(index)<=47) ||
-                    ((int)value.charAt(index)>=58 &&
-                     (int)value.charAt(index)<=64) ||
-                    ((int)value.charAt(index)>=91 &&
-                     (int)value.charAt(index)<=96) ||
-                    ((int)value.charAt(index)>=123 &&
-                     (int)value.charAt(index)<=126)
-            )
-            {
-                return true;
-            }
-        }
-        return false;
-    }
 }
