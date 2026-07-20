cb881587dfcc2ce6ac2c706e5a945590c447fef0
albashzia <ahmedalbashzia@gmail.com>
2026-07-20 23:51:56 +0500

    Define the checkPassword() method in PasswordChecker class to check the password given and return an array of boolean type returning the results of validate methods of various checking rule

--- src/Code/PasswordChecker.java
+++ src/Code/PasswordChecker.java
@@ -3,8 +3,16 @@ package Code;
 import Code.Rules.CommonPasswordRule;
 
 public class PasswordChecker {
-
+    CommonPasswordRule commonPasswordRule;
     PasswordChecker(){
-        CommonPasswordRule commonPasswordRule = new CommonPasswordRule();
+        commonPasswordRule = new CommonPasswordRule();
+    }
+
+    public boolean[] checkPassword(Password password){
+        boolean[] result = new boolean[8];
+
+        result[0] = commonPasswordRule.validate(password);
+
+        return result;
     }
 }
