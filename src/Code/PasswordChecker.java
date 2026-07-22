3ceb3f6313f7ad8938df9945dacf52223bbb4416
albashzia <ahmedalbashzia@gmail.com>
2026-07-22 13:08:16 +0500

    Add an instance of LowercaseRule class to the constructor of PasswordChecker class

--- src/Code/PasswordChecker.java
+++ src/Code/PasswordChecker.java
@@ -3,16 +3,19 @@ package Code;
 import Code.Rules.CommonPasswordRule;
 import Code.Rules.DigitRule;
 import Code.Rules.LengthRule;
+import Code.Rules.LowercaseRule;
 
 public class PasswordChecker {
     CommonPasswordRule commonPasswordRule;
     DigitRule digitRule;
     LengthRule lengthRule;
+    LowercaseRule lowercaseRule;
 
     PasswordChecker(){
         commonPasswordRule = new CommonPasswordRule();
         digitRule = new DigitRule();
         lengthRule = new LengthRule();
+        lowercaseRule = new LowercaseRule();
     }
 
     public boolean[] checkPassword(Password password){
