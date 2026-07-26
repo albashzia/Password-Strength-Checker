0f79627c53987d41af7b97ce54e67af23646e512
albashzia <ahmedalbashzia@gmail.com>
2026-07-26 23:01:38 +0500

    Create objects of Password assessment rules inside the method of PolicyFactory class

--- src/Code/PolicyFactory.java
+++ src/Code/PolicyFactory.java
@@ -2,9 +2,20 @@ package Code;
 
 //Creates policies based on user choice
 
+import Code.Rules.DigitRule;
+import Code.Rules.LengthRule;
+import Code.Rules.LowercaseRule;
+import Code.Rules.UppercaseRule;
+
 public class PolicyFactory {
 
-    void createBasicPolicy(){};
+    void createBasicPolicy(Password password){
+        DigitRule digitRule = new DigitRule();
+        LengthRule lengthRule = new LengthRule();
+        UppercaseRule uppercaseRule = new UppercaseRule();
+        LowercaseRule lowercaseRule = new LowercaseRule();
+
+    };
     void createIntermediatePolicy(){};
     void createStrongPolicy(){};
     void createAdvancedPolicy(){};
