b97a9ac0ab5a23f80873247d533d129a4cc1fb1c
albashzia <ahmedalbashzia@gmail.com>
2026-08-11 17:08:22 +0500

    Defining the attributes of PasswordPolicy class

--- src/Code/PasswordPolicy.java
+++ src/Code/PasswordPolicy.java
@@ -1,4 +1,11 @@
 package Code;
 
+import Code.Rules.PasswordRule;
+
+import java.util.ArrayList;
+
 public class PasswordPolicy {
+    private String policyName;
+    private ArrayList<PasswordRule> rules;
+
 }
