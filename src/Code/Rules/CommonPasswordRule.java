e54186620defb02008e5a37b1b3177b736c52762
albashzia <ahmedalbashzia@gmail.com>
2026-09-13 19:04:30 +0500

    Add a method to load common passwords from text file

--- src/Code/Rules/CommonPasswordRule.java
+++ src/Code/Rules/CommonPasswordRule.java
@@ -2,68 +2,32 @@ package Code.Rules;
 
 import Code.Password;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CommonPasswordRule extends PasswordRule{
 
-    ArrayList<String> commonPasswords = new ArrayList<>(Arrays.asList(
-            "123456",
-            "123456789",
-            "12345678",
-            "12345",
-            "1234567",
-            "password",
-            "password123",
-            "admin",
-            "admin123",
-            "qwerty",
-            "qwerty123",
-            "abc123",
-            "111111",
-            "123123",
-            "000000",
-            "iloveyou",
-            "welcome",
-            "monkey",
-            "login",
-            "letmein",
-            "football",
-            "dragon",
-            "master",
-            "sunshine",
-            "ashley",
-            "bailey",
-            "shadow",
-            "superman",
-            "harley",
-            "password1",
-            "trustno1",
-            "hello",
-            "freedom",
-            "whatever",
-            "qazwsx",
-            "starwars",
-            "passw0rd",
-            "654321",
-            "7777777",
-            "1q2w3e4r",
-            "zaq12wsx",
-            "google",
-            "facebook",
-            "youtube",
-            "linkedin",
-            "admin@123",
-            "root",
-            "toor",
-            "guest",
-            "test",
-            "user",
-            "default"
-    ));
+    ArrayList<String> commonPasswords = new ArrayList<>();
+
 
     public CommonPasswordRule() {
 
+    }
+
+    private void loadCommonPasswords() {
+        try {
+            String data = Files.readString(Path.of("Resources/common_passwords.txt"));
+
+            for (String password : data.split(",")) {
+                commonPasswords.add(password.trim());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Could not read common_passwords.txt");
+        }
     }
 
     public CommonPasswordRule(String ruleName, String errorMessage){
